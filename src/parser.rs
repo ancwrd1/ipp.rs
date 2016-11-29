1f5bd8b9fb6f400d0e4419fd09b5eb8aac125977
Dmitry Pankratov <dp@ubiquitech.com>
2016-11-29 09:00:13 +0100

    Code comments

--- src/parser.rs
+++ src/parser.rs
@@ -45,14 +45,23 @@ impl<'a> IppParser<'a> {
 
     /// Parse IPP stream
     pub fn parse(&mut self) -> Result<IppParseResult> {
+        // last delimiter tag
         let mut delimiter = 0;
+
+        // stack of current attributes context. Used with lists and collections
         let mut stack: Vec<Vec<IppValue>> = Vec::new();
+
+        // holds the result of parsing
         let mut retval: IppAttributeList = IppAttributeList::new();
+
+        // tuple of previous attribute (tag, name)
         let mut prev = (0, String::new());
 
+        // parse IPP header
         let header = IppHeader::from_reader(self.reader)?;
         debug!("IPP reply header: {:?}", header);
 
+        // start with new collection
         stack.push(Vec::new());
 
         loop {
@@ -60,11 +69,13 @@ impl<'a> IppParser<'a> {
             if is_delimiter_tag(tag) {
                 debug!("Delimiter tag: {:0x}", tag);
                 if tag == END_OF_ATTRIBUTES_TAG {
+                    // end of stream, get last saved collection
                     let mut val_list = stack.pop().unwrap();
                     let v = if val_list.len() == 1 {val_list.remove(0)} else {IppValue::ListOf(val_list)};
                     retval.add(delimiter, IppAttribute::new(&prev.1, v));
                     break;
                 } else {
+                    // remember delimiter tag
                     delimiter = tag;
                     continue;
                 }
@@ -77,27 +88,32 @@ impl<'a> IppParser<'a> {
                 debug!("Value tag: {:0x}: {}: {}", tag, name, value);
 
                 if namelen > 0 {
-                    // single attribute
+                    // single attribute or begin of array
                     if prev.0 != 0 {
+                        // put the previous attribute into the retval
                         let mut val_list = stack.pop().unwrap();
                         let v = if val_list.len() == 1 {val_list.remove(0)} else {IppValue::ListOf(val_list)};
                         retval.add(delimiter, IppAttribute::new(&prev.1, v));
                         stack.push(Vec::new());
                     }
+                    // store it as a previous attribute
                     prev = (tag, name);
                 }
                 match tag {
                     BEG_COLLECTION => {
+                        // start new collection in the stack
                         debug!("Begin collection");
                         stack.push(Vec::new())
                     }
                     END_COLLECTION => {
+                        // get collection from the stack and add it to the previous element
                         debug!("End collection");
                         let arr = stack.pop().unwrap();
                         let mut val_list = stack.last_mut().unwrap();
                         val_list.push(IppValue::Collection(arr));
                     }
                     _ => {
+                        // add attribute to the current collection
                         stack.last_mut().unwrap().push(value);
                     }
                 }
