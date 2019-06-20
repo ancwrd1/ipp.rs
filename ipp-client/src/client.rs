ce6a7e97088a09d5e4361d7b3bf4e3b893ce9741
Dmitry Pankratov <dmitry@pankratov.net>
2019-06-20 13:40:36 +0200

    Updated IppClient::send_request

--- ipp-client/src/client.rs
+++ ipp-client/src/client.rs
@@ -40,6 +40,49 @@ const ERROR_STATES: &[&str] = &[
     "shutdown",
 ];
 
+fn parse_uri(uri: String) -> impl Future<Item = Url, Error = IppError> {
+    futures::lazy(move || match Url::parse(&uri) {
+        Ok(mut url) => {
+            match url.scheme() {
+                "ipp" => {
+                    url.set_scheme("http").unwrap();
+                    if url.port().is_none() {
+                        url.set_port(Some(631)).unwrap();
+                    }
+                }
+                "ipps" => {
+                    url.set_scheme("https").unwrap();
+                    if url.port().is_none() {
+                        url.set_port(Some(443)).unwrap();
+                    }
+                }
+                _ => {}
+            }
+            Ok(url)
+        }
+        Err(e) => Err(IppError::ParamError(e.to_string())),
+    })
+}
+
+fn parse_certs(certs: Vec<PathBuf>) -> impl Future<Item = Vec<Certificate>, Error = IppError> {
+    futures::lazy(move || {
+        let mut result = Vec::new();
+
+        for cert_file in certs {
+            let buf = match fs::read(&cert_file) {
+                Ok(buf) => buf,
+                Err(e) => return Err(IppError::from(e)),
+            };
+            let ca_cert = match Certificate::from_der(&buf).or_else(|_| Certificate::from_pem(&buf)) {
+                Ok(ca_cert) => ca_cert,
+                Err(e) => return Err(IppError::from(e)),
+            };
+            result.push(ca_cert);
+        }
+        Ok(result)
+    })
+}
+
 /// IPP client.
 ///
 /// IPP client is responsible for sending requests to IPP server.
@@ -119,45 +162,9 @@ impl IppClient {
     pub fn send_request(
         &self,
         request: IppRequestResponse,
-    ) -> Box<dyn Future<Item = IppRequestResponse, Error = IppError> + Send> {
-        let url = match Url::parse(&self.uri) {
-            Ok(mut url) => {
-                match url.scheme() {
-                    "ipp" => {
-                        url.set_scheme("http").unwrap();
-                        if url.port().is_none() {
-                            url.set_port(Some(631)).unwrap();
-                        }
-                    }
-                    "ipps" => {
-                        url.set_scheme("https").unwrap();
-                        if url.port().is_none() {
-                            url.set_port(Some(631)).unwrap();
-                        }
-                    }
-                    _ => {}
-                }
-                url
-            }
-            Err(e) => return Box::new(Err(IppError::ParamError(e.to_string())).into_future()),
-        };
-
-        debug!("Request URI: {}", url);
-
+    ) -> impl Future<Item = IppRequestResponse, Error = IppError> + Send {
         // Some printers don't support gzip
         let mut builder = Client::builder().gzip(false).connect_timeout(Duration::from_secs(10));
-
-        for cert_file in &self.ca_certs {
-            let buf = match fs::read(&cert_file) {
-                Ok(buf) => buf,
-                Err(e) => return Box::new(Err(IppError::from(e)).into_future()),
-            };
-            let ca_cert = match Certificate::from_der(&buf).or_else(|_| Certificate::from_pem(&buf)) {
-                Ok(ca_cert) => ca_cert,
-                Err(e) => return Box::new(Err(IppError::from(e)).into_future()),
-            };
-            builder = builder.add_root_certificate(ca_cert);
-        }
 
         if !self.verify_hostname {
             debug!("Disabling hostname verification!");
@@ -174,28 +181,39 @@ impl IppClient {
             builder = builder.timeout(Duration::from_secs(self.timeout));
         }
 
-        let fut = builder
-            .build()
-            .into_future()
-            .and_then(|client| {
-                client
-                    .post(url)
-                    .header("Content-Type", "application/ipp")
-                    .body(request.into_stream())
-                    .send()
-            })
-            .and_then(|response| response.error_for_status())
-            .and_then(|mut response| {
-                let body = mem::replace(response.body_mut(), Decoder::empty());
-                body.concat2()
-            })
-            .map_err(IppError::HttpError)
-            .and_then(|body| {
-                let mut reader = BufReader::new(Cursor::new(body));
-                let parser = IppParser::new(&mut reader);
-                IppRequestResponse::from_parser(parser).map_err(IppError::ParseError)
-            });
+        let uri = self.uri.clone();
+        let ca_certs = self.ca_certs.clone();
 
-        Box::new(fut)
+        parse_uri(uri).and_then(|url| {
+            parse_certs(ca_certs).and_then(|certs| {
+                debug!("Request URI: {}", url);
+
+                for ca_cert in certs {
+                    builder = builder.add_root_certificate(ca_cert);
+                }
+
+                builder
+                    .build()
+                    .into_future()
+                    .and_then(|client| {
+                        client
+                            .post(url)
+                            .header("Content-Type", "application/ipp")
+                            .body(request.into_stream())
+                            .send()
+                    })
+                    .and_then(|response| response.error_for_status())
+                    .and_then(|mut response| {
+                        let body = mem::replace(response.body_mut(), Decoder::empty());
+                        body.concat2()
+                    })
+                    .map_err(IppError::HttpError)
+                    .and_then(|body| {
+                        let mut reader = BufReader::new(Cursor::new(body));
+                        let parser = IppParser::new(&mut reader);
+                        IppRequestResponse::from_parser(parser).map_err(IppError::ParseError)
+                    })
+            })
+        })
     }
 }
