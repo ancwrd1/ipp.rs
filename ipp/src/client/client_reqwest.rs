02a6002cb337c30aafe9ed2e2b777dd2fd1275dd
Dmitry Pankratov <dmitry@pankratov.net>
2020-10-05 11:32:22 +0200

    Use provided conversion from Stream to AsyncRead

--- ipp/src/client/client_reqwest.rs
+++ ipp/src/client/client_reqwest.rs
@@ -1,4 +1,6 @@
-use futures_util::io::BufReader;
+use std::io;
+
+use futures_util::{io::BufReader, stream::TryStreamExt};
 use log::debug;
 use reqwest::{Body, ClientBuilder};
 
@@ -43,10 +45,15 @@ impl<'a> ReqwestClient<'a> {
         debug!("Response status: {}", response.status());
 
         match response.status().as_u16() {
-            200..=202 => IppParser::new(BufReader::new(util::StreamReader::new(response.bytes_stream())))
-                .parse()
-                .await
-                .map_err(IppError::from),
+            200..=202 => IppParser::new(BufReader::new(
+                response
+                    .bytes_stream()
+                    .map_err(|e| io::Error::new(io::ErrorKind::BrokenPipe, e))
+                    .into_async_read(),
+            ))
+            .parse()
+            .await
+            .map_err(IppError::from),
             other => Err(IppError::RequestError(other)),
         }
     }
@@ -54,75 +61,15 @@ impl<'a> ReqwestClient<'a> {
 
 mod util {
     use std::{
-        cmp, io,
+        io,
         pin::Pin,
         task::{Context, Poll},
     };
 
-    use bytes::Buf;
     use futures_util::{io::AsyncRead, stream::Stream};
     use pin_project::pin_project;
 
     const CHUNK_SIZE: usize = 32768;
-
-    #[pin_project]
-    pub(super) struct StreamReader<S, B> {
-        #[pin]
-        inner: S,
-        chunk: Option<B>,
-    }
-
-    impl<S, B, E> StreamReader<S, B>
-    where
-        S: Stream<Item = Result<B, E>>,
-        B: Buf,
-        E: Into<reqwest::Error>,
-    {
-        pub fn new(stream: S) -> StreamReader<S, B> {
-            StreamReader {
-                inner: stream,
-                chunk: None,
-            }
-        }
-
-        fn has_chunk(self: Pin<&mut Self>) -> bool {
-            if let Some(chunk) = self.project().chunk {
-                chunk.remaining() > 0
-            } else {
-                false
-            }
-        }
-    }
-
-    impl<S, B, E> AsyncRead for StreamReader<S, B>
-    where
-        S: Stream<Item = Result<B, E>>,
-        B: Buf,
-        E: Into<reqwest::Error>,
-    {
-        fn poll_read(mut self: Pin<&mut Self>, cx: &mut Context, buf: &mut [u8]) -> Poll<io::Result<usize>> {
-            loop {
-                if self.as_mut().has_chunk() {
-                    let chunk = self.project().chunk.as_mut().unwrap();
-                    let len = cmp::min(chunk.remaining(), buf.len());
-                    buf[..len].copy_from_slice(&chunk.bytes()[..len]);
-                    chunk.advance(len);
-                    return Poll::Ready(Ok(len));
-                } else {
-                    let me = self.as_mut().project();
-                    match futures_util::ready!(me.inner.poll_next(cx)) {
-                        Some(Ok(chunk)) => {
-                            *me.chunk = Some(chunk);
-                        }
-                        Some(Err(err)) => {
-                            return Poll::Ready(Err(io::Error::new(io::ErrorKind::BrokenPipe, err.into())))
-                        }
-                        None => return Poll::Ready(Ok(0)),
-                    }
-                }
-            }
-        }
-    }
 
     #[pin_project]
     pub(super) struct ReaderStream<R> {
@@ -151,42 +98,5 @@ mod util {
                 Err(e) => Poll::Ready(Some(Err(e))),
             }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use bytes::Bytes;
-    use futures_util::{io::AsyncReadExt, stream::iter};
-
-    #[test]
-    fn test_stream_reader() {
-        let _ = futures::executor::block_on(async {
-            let stream = iter(vec![
-                Result::Ok::<_, reqwest::Error>(Bytes::from_static(&[])),
-                Ok(Bytes::from_static(&[0, 1, 2, 3])),
-                Ok(Bytes::from_static(&[])),
-                Ok(Bytes::from_static(&[4, 5, 6, 7])),
-                Ok(Bytes::from_static(&[])),
-                Ok(Bytes::from_static(&[8, 9, 10, 11])),
-                Ok(Bytes::from_static(&[])),
-            ]);
-
-            let mut read = super::util::StreamReader::new(stream);
-
-            let mut buf = [0; 5];
-            read.read_exact(&mut buf).await?;
-            assert_eq!(buf, [0, 1, 2, 3, 4]);
-
-            assert_eq!(read.read(&mut buf).await?, 3);
-            assert_eq!(&buf[..3], [5, 6, 7]);
-
-            assert_eq!(read.read(&mut buf).await?, 4);
-            assert_eq!(&buf[..4], [8, 9, 10, 11]);
-
-            assert_eq!(read.read(&mut buf).await?, 0);
-
-            Ok::<(), std::io::Error>(())
-        });
     }
 }
