20d34428d75ee30edd0ab1d45ccdd581aea02f87
Dmitry Pankratov <dmitry@pankratov.net>
2022-04-13 11:39:53 +0200

    Updated documentation

--- doc/implementors/std/io/trait.Read.js
+++ doc/implementors/std/io/trait.Read.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["ipp"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"ipp/payload/struct.IppPayload.html\" title=\"struct ipp::payload::IppPayload\">IppPayload</a>","synthetic":false,"types":["ipp::payload::IppPayload"]}];
+implementors["ipp"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"ipp/payload/struct.IppPayload.html\" title=\"struct ipp::payload::IppPayload\">IppPayload</a>","synthetic":false,"types":["ipp::payload::IppPayload"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
