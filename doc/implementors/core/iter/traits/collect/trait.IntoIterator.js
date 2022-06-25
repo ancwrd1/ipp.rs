56d1cd70986092f106b6bbfe7dd73b2c63c276a7
Dmitry Pankratov <dmitry@pankratov.net>
2022-06-25 20:30:25 +0200

    Updated documentation

--- doc/implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ doc/implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["ipp"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"enum\" href=\"ipp/value/enum.IppValue.html\" title=\"enum ipp::value::IppValue\">IppValue</a>","synthetic":false,"types":["ipp::value::IppValue"]}];
+implementors["ipp"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"enum\" href=\"ipp/value/enum.IppValue.html\" title=\"enum ipp::value::IppValue\">IppValue</a>","synthetic":false,"types":["ipp::value::IppValue"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
