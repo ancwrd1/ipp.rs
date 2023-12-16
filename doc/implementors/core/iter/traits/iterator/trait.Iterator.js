ba2602ce5bebe8e5ceba1b49def9f90641b73c92
Dmitry Pankratov <dmitry@pankratov.net>
2023-12-16 13:55:44 +0100

    Updated documentation

--- doc/implementors/core/iter/traits/iterator/trait.Iterator.js
+++ doc/implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ipp":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"ipp/value/struct.IppValueIterator.html\" title=\"struct ipp::value::IppValueIterator\">IppValueIterator</a>&lt;'a&gt;"]]
+"ipp":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"ipp/value/struct.IppValueIterator.html\" title=\"struct ipp::value::IppValueIterator\">IppValueIterator</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
