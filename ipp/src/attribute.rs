47d7a014d7967c03079885c609344ccfdb591f8c
Dmitry Pankratov <dmitry@pankratov.net>
2026-02-11 17:55:22 +0100

    Refactored attribute consts to use macro

--- ipp/src/attribute.rs
+++ ipp/src/attribute.rs
@@ -9,6 +9,12 @@ use serde::{Deserialize, Serialize};
 
 use crate::{model::DelimiterTag, value::IppValue};
 
+macro_rules! define_attributes {
+    ($($name:ident => $value:literal),* $(,)?) => {
+        $(pub const $name: &'static str = $value;)*
+    };
+}
+
 fn is_header_attr(attr: &str) -> bool {
     IppAttribute::HEADER_ATTRS.contains(&attr)
 }
@@ -24,90 +30,92 @@ pub struct IppAttribute {
 }
 
 impl IppAttribute {
-    pub const ATTRIBUTES_CHARSET: &'static str = "attributes-charset";
-    pub const ATTRIBUTES_NATURAL_LANGUAGE: &'static str = "attributes-natural-language";
-    pub const CHARSET_CONFIGURED: &'static str = "charset-configured";
-    pub const CHARSET_SUPPORTED: &'static str = "charset-supported";
-    pub const COLOR_MODE_SUPPORTED: &'static str = "color-mode-supported";
-    pub const COLOR_SUPPORTED: &'static str = "color-supported";
-    pub const COMPRESSION_SUPPORTED: &'static str = "compression-supported";
-    pub const COPIES: &'static str = "copies";
-    pub const COPIES_DEFAULT: &'static str = "copies-default";
-    pub const COPIES_SUPPORTED: &'static str = "copies-supported";
-    pub const DOCUMENT_FORMAT: &'static str = "document-format";
-    pub const DOCUMENT_FORMAT_DEFAULT: &'static str = "document-format-default";
-    pub const DOCUMENT_FORMAT_PREFERRED: &'static str = "document-format-preferred";
-    pub const DOCUMENT_FORMAT_SUPPORTED: &'static str = "document-format-supported";
-    pub const FINISHINGS: &'static str = "finishings";
-    pub const FINISHINGS_DEFAULT: &'static str = "finishings-default";
-    pub const FINISHINGS_SUPPORTED: &'static str = "finishings-supported";
-    pub const GENERATED_NATURAL_LANGUAGE_SUPPORTED: &'static str = "generated-natural-language-supported";
-    pub const IPP_VERSIONS_SUPPORTED: &'static str = "ipp-versions-supported";
-    pub const JOB_ID: &'static str = "job-id";
-    pub const JOB_NAME: &'static str = "job-name";
-    pub const JOB_STATE: &'static str = "job-state";
-    pub const JOB_STATE_REASONS: &'static str = "job-state-reasons";
-    pub const JOB_URI: &'static str = "job-uri";
-    pub const LAST_DOCUMENT: &'static str = "last-document";
-    pub const MEDIA_COL: &'static str = "media-col";
-    pub const MEDIA_COL_DATABASE: &'static str = "media-col-database";
-    pub const MEDIA_COL_DEFAULT: &'static str = "media-col-default";
-    pub const MEDIA_COL_READY: &'static str = "media-col-ready";
-    pub const MEDIA_COL_SUPPORTED: &'static str = "media-col-supported";
-    pub const MEDIA_DEFAULT: &'static str = "media-default";
-    pub const MEDIA_READY: &'static str = "media-ready";
-    pub const MEDIA_SOURCE_SUPPORTED: &'static str = "media-source-supported";
-    pub const MEDIA_SUPPORTED: &'static str = "media-supported";
-    pub const MEDIA_TYPE_SUPPORTED: &'static str = "media-type-supported";
-    pub const MOPRIA_CERTIFIED: &'static str = "mopria-certified";
-    pub const MULTIPLE_DOCUMENT_HANDLING: &'static str = "multiple-document-handling";
-    pub const MULTIPLE_DOCUMENT_HANDLING_DEFAULT: &'static str = "multiple-document-handling-default";
-    pub const MULTIPLE_DOCUMENT_HANDLING_SUPPORTED: &'static str = "multiple-document-handling-supported";
-    pub const NATURAL_LANGUAGE_CONFIGURED: &'static str = "natural-language-configured";
-    pub const OPERATIONS_SUPPORTED: &'static str = "operations-supported";
-    pub const ORIENTATION_REQUESTED: &'static str = "orientation-requested";
-    pub const ORIENTATION_REQUESTED_DEFAULT: &'static str = "orientation-requested-default";
-    pub const ORIENTATION_REQUESTED_SUPPORTED: &'static str = "orientation-requested-supported";
-    pub const OUTPUT_BIN: &'static str = "output-bin";
-    pub const OUTPUT_BIN_DEFAULT: &'static str = "output-bin-default";
-    pub const OUTPUT_BIN_SUPPORTED: &'static str = "output-bin-supported";
-    pub const OUTPUT_MODE_SUPPORTED: &'static str = "output-mode-supported";
-    pub const PAGES_PER_MINUTE: &'static str = "pages-per-minute";
-    pub const PDL_OVERRIDE_SUPPORTED: &'static str = "pdl-override-supported";
-    pub const PRINTER_DEVICE_ID: &'static str = "printer-device-id";
-    pub const PRINTER_FIRMWARE_NAME: &'static str = "printer-firmware-name";
-    pub const PRINTER_FIRMWARE_STRING_VERSION: &'static str = "printer-firmware-string-version";
-    pub const PRINTER_INFO: &'static str = "printer-info";
-    pub const PRINTER_IS_ACCEPTING_JOBS: &'static str = "printer-is-accepting-jobs";
-    pub const PRINTER_LOCATION: &'static str = "printer-location";
-    pub const PRINTER_MAKE_AND_MODEL: &'static str = "printer-make-and-model";
-    pub const PRINTER_MORE_INFO: &'static str = "printer-more-info";
-    pub const PRINTER_NAME: &'static str = "printer-name";
-    pub const PRINTER_RESOLUTION: &'static str = "printer-resolution";
-    pub const PRINTER_RESOLUTION_DEFAULT: &'static str = "printer-resolution-default";
-    pub const PRINTER_RESOLUTION_SUPPORTED: &'static str = "printer-resolution-supported";
-    pub const PRINTER_STATE: &'static str = "printer-state";
-    pub const PRINTER_STATE_MESSAGE: &'static str = "printer-state-message";
-    pub const PRINTER_STATE_REASONS: &'static str = "printer-state-reasons";
-    pub const PRINTER_UP_TIME: &'static str = "printer-up-time";
-    pub const PRINTER_URI: &'static str = "printer-uri";
-    pub const PRINTER_URI_SUPPORTED: &'static str = "printer-uri-supported";
-    pub const PRINTER_UUID: &'static str = "printer-uuid";
-    pub const PRINT_COLOR_MODE: &'static str = "print-color-mode";
-    pub const PRINT_COLOR_MODE_DEFAULT: &'static str = "print-color-mode-default";
-    pub const PRINT_COLOR_MODE_SUPPORTED: &'static str = "print-color-mode-supported";
-    pub const PRINT_QUALITY: &'static str = "print-quality";
-    pub const PRINT_QUALITY_DEFAULT: &'static str = "print-quality-default";
-    pub const PRINT_QUALITY_SUPPORTED: &'static str = "print-quality-supported";
-    pub const QUEUED_JOB_COUNT: &'static str = "queued-job-count";
-    pub const REQUESTED_ATTRIBUTES: &'static str = "requested-attributes";
-    pub const REQUESTING_USER_NAME: &'static str = "requesting-user-name";
-    pub const SIDES: &'static str = "sides";
-    pub const SIDES_DEFAULT: &'static str = "sides-default";
-    pub const SIDES_SUPPORTED: &'static str = "sides-supported";
-    pub const STATUS_MESSAGE: &'static str = "status-message";
-    pub const URI_AUTHENTICATION_SUPPORTED: &'static str = "uri-authentication-supported";
-    pub const URI_SECURITY_SUPPORTED: &'static str = "uri-security-supported";
+    define_attributes! {
+        ATTRIBUTES_CHARSET => "attributes-charset",
+        ATTRIBUTES_NATURAL_LANGUAGE => "attributes-natural-language",
+        CHARSET_CONFIGURED => "charset-configured",
+        CHARSET_SUPPORTED => "charset-supported",
+        COLOR_MODE_SUPPORTED => "color-mode-supported",
+        COLOR_SUPPORTED => "color-supported",
+        COMPRESSION_SUPPORTED => "compression-supported",
+        COPIES => "copies",
+        COPIES_DEFAULT => "copies-default",
+        COPIES_SUPPORTED => "copies-supported",
+        DOCUMENT_FORMAT => "document-format",
+        DOCUMENT_FORMAT_DEFAULT => "document-format-default",
+        DOCUMENT_FORMAT_PREFERRED => "document-format-preferred",
+        DOCUMENT_FORMAT_SUPPORTED => "document-format-supported",
+        FINISHINGS => "finishings",
+        FINISHINGS_DEFAULT => "finishings-default",
+        FINISHINGS_SUPPORTED => "finishings-supported",
+        GENERATED_NATURAL_LANGUAGE_SUPPORTED => "generated-natural-language-supported",
+        IPP_VERSIONS_SUPPORTED => "ipp-versions-supported",
+        JOB_ID => "job-id",
+        JOB_NAME => "job-name",
+        JOB_STATE => "job-state",
+        JOB_STATE_REASONS => "job-state-reasons",
+        JOB_URI => "job-uri",
+        LAST_DOCUMENT => "last-document",
+        MEDIA_COL => "media-col",
+        MEDIA_COL_DATABASE => "media-col-database",
+        MEDIA_COL_DEFAULT => "media-col-default",
+        MEDIA_COL_READY => "media-col-ready",
+        MEDIA_COL_SUPPORTED => "media-col-supported",
+        MEDIA_DEFAULT => "media-default",
+        MEDIA_READY => "media-ready",
+        MEDIA_SOURCE_SUPPORTED => "media-source-supported",
+        MEDIA_SUPPORTED => "media-supported",
+        MEDIA_TYPE_SUPPORTED => "media-type-supported",
+        MOPRIA_CERTIFIED => "mopria-certified",
+        MULTIPLE_DOCUMENT_HANDLING => "multiple-document-handling",
+        MULTIPLE_DOCUMENT_HANDLING_DEFAULT => "multiple-document-handling-default",
+        MULTIPLE_DOCUMENT_HANDLING_SUPPORTED => "multiple-document-handling-supported",
+        NATURAL_LANGUAGE_CONFIGURED => "natural-language-configured",
+        OPERATIONS_SUPPORTED => "operations-supported",
+        ORIENTATION_REQUESTED => "orientation-requested",
+        ORIENTATION_REQUESTED_DEFAULT => "orientation-requested-default",
+        ORIENTATION_REQUESTED_SUPPORTED => "orientation-requested-supported",
+        OUTPUT_BIN => "output-bin",
+        OUTPUT_BIN_DEFAULT => "output-bin-default",
+        OUTPUT_BIN_SUPPORTED => "output-bin-supported",
+        OUTPUT_MODE_SUPPORTED => "output-mode-supported",
+        PAGES_PER_MINUTE => "pages-per-minute",
+        PDL_OVERRIDE_SUPPORTED => "pdl-override-supported",
+        PRINTER_DEVICE_ID => "printer-device-id",
+        PRINTER_FIRMWARE_NAME => "printer-firmware-name",
+        PRINTER_FIRMWARE_STRING_VERSION => "printer-firmware-string-version",
+        PRINTER_INFO => "printer-info",
+        PRINTER_IS_ACCEPTING_JOBS => "printer-is-accepting-jobs",
+        PRINTER_LOCATION => "printer-location",
+        PRINTER_MAKE_AND_MODEL => "printer-make-and-model",
+        PRINTER_MORE_INFO => "printer-more-info",
+        PRINTER_NAME => "printer-name",
+        PRINTER_RESOLUTION => "printer-resolution",
+        PRINTER_RESOLUTION_DEFAULT => "printer-resolution-default",
+        PRINTER_RESOLUTION_SUPPORTED => "printer-resolution-supported",
+        PRINTER_STATE => "printer-state",
+        PRINTER_STATE_MESSAGE => "printer-state-message",
+        PRINTER_STATE_REASONS => "printer-state-reasons",
+        PRINTER_UP_TIME => "printer-up-time",
+        PRINTER_URI => "printer-uri",
+        PRINTER_URI_SUPPORTED => "printer-uri-supported",
+        PRINTER_UUID => "printer-uuid",
+        PRINT_COLOR_MODE => "print-color-mode",
+        PRINT_COLOR_MODE_DEFAULT => "print-color-mode-default",
+        PRINT_COLOR_MODE_SUPPORTED => "print-color-mode-supported",
+        PRINT_QUALITY => "print-quality",
+        PRINT_QUALITY_DEFAULT => "print-quality-default",
+        PRINT_QUALITY_SUPPORTED => "print-quality-supported",
+        QUEUED_JOB_COUNT => "queued-job-count",
+        REQUESTED_ATTRIBUTES => "requested-attributes",
+        REQUESTING_USER_NAME => "requesting-user-name",
+        SIDES => "sides",
+        SIDES_DEFAULT => "sides-default",
+        SIDES_SUPPORTED => "sides-supported",
+        STATUS_MESSAGE => "status-message",
+        URI_AUTHENTICATION_SUPPORTED => "uri-authentication-supported",
+        URI_SECURITY_SUPPORTED => "uri-security-supported",
+    }
 
     // Per section 4.1.4. Character Set and Natural Language Operation Attributes
     // The "attributes-charset" and "attributes-natural-language" attributes MUST be the first two attributes
